78ea17c37ffe452d1bae2315f11acf0dc7a292a9
Noah <noahhughesph@gmail.com>
2023-09-14 23:23:10 -0400

    props hate me

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
-import AdminNavBar from "./AdminNavBar";
-import QuestionForm from "./QuestionForm";
-import QuestionList from "./QuestionList";
+import React, { useState, useEffect } from 'react';
+import AdminNavBar from './AdminNavBar';
+import QuestionForm from './QuestionForm';
+import QuestionList from './QuestionList';
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [page, setPage] = useState('List');
+  const [questions, setQuestions] = useState([]);
 
+  useEffect(() => {
+    const questionsFetch = async () => {
+      const resp = await fetch('http://localhost:4000/questions');
+      const data = await resp.json();
+      setQuestions(data);
+    };
+    questionsFetch();
+  }, []);
+
+  const handleNewQuestion = (newQuestion) => {
+    setQuestions([...questions, newQuestion]);
+  };
+
+  const handleDelete = (questionId) => {
+    const newQuestions = questions.filter((question) => {
+      return question.id !== questionId;
+    });
+    setQuestions(newQuestions);
+  };
+
+  console.log(questions);
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === 'Form' ? (
+        <QuestionForm handleNewQuestion={handleNewQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          handleDelete={handleDelete}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,20 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+  const handleQuestionDelete = () => {
+    const deleteRequest = async () => {
+      const resp = await fetch(
+        `http://localhost:4000/questions/${id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await resp.json();
+      handleDelete(id);
+    };
+    deleteRequest();
+  };
 
   return (
     <li>
@@ -17,7 +31,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleQuestionDelete}>Delete Question</button>
     </li>
   );
 }
